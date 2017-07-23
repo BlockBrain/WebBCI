1d2bacc60c8ccb5318525b3a9c110bc89bdcb982
cooga011 <c.coogan2201@gmail.com>
2017-07-23 13:54:50 -0500

    index,networking,style

---
+++ MultiNetServer.js
@@ -0,0 +1,70 @@
+// Load required modules
+var http    = require("http");              // http server core module
+var app = require("express")();           // web framework external module
+var serveStatic = require('serve-static');  // serve static files
+var socketIo = require("socket.io");        // web socket external module
+var easyrtc = require("easyrtc");               // EasyRTC external module
+var dgram = require('dgram')
+const udpServer = dgram.createSocket('udp4');
+
+
+app.use(serveStatic(__dirname, {'index': ['index.html']}));
+
+var webServer = http.createServer(app);
+
+udpServer.bind(55404,"192.168.0.6");
+
+var CPosX;
+var CPosY;
+
+udpServer.on('message', (msg1, rinfo) => {
+  var BCIString = String.fromCharCode.apply(null, new Uint16Array(msg1))
+  var CPx = BCIString.indexOf("CursorPosX");
+  var CPy = BCIString.indexOf("CursorPosY");
+  var TarC = BCIString.indexOf("TargetCode");
+  var ResC = BCIString.indexOf("ResultCode");
+  if(CPx == 0){
+    CPosX=parseFloat(BCIString.substring(11,15));
+  }
+  if(CPy == 0){
+    CPosY=parseFloat(BCIString.substring(11,15));
+  }
+  if(CPy == 0 || CPx == 0){
+    socketServer.emit('Pos',{ PosX: (CPosX-1548)/387, PosY: ((CPosY-1548)/290)+6.15});
+//    console.log("Cursor position X: ");
+  //  console.log((CPosX));
+    //console.log("Cursor position Y: ");
+    console.log((CPosY));
+  }
+});
+
+var socketServer = socketIo.listen(webServer, {"log level":1});
+
+var myIceServers = [
+  {"url":"stun:stun.l.google.com:19302"},
+  {"url":"stun:stun1.l.google.com:19302"},
+  {"url":"stun:stun2.l.google.com:19302"},
+  {"url":"stun:stun3.l.google.com:19302"}
+  // {
+  //   "url":"turn:[ADDRESS]:[PORT]",
+  //   "username":"[USERNAME]",
+  //   "credential":"[CREDENTIAL]"
+  // },
+  // {
+  //   "url":"turn:[ADDRESS]:[PORT][?transport=tcp]",
+  //   "username":"[USERNAME]",
+  //   "credential":"[CREDENTIAL]"
+  // }
+];
+
+easyrtc.setOption("appIceServers", myIceServers);
+easyrtc.setOption("demosEnable", false);
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    });
+});
+
+//listen on port
+webServer.listen(80, function () {});
