c45dd267f5581152e0cff448122a4437ee665d78
Chris <c.coogan2201@gmail.com>
2017-07-28 21:58:21 -0500

    chat page

--- MultiNetServer.js
+++ MultiNetServer.js
@@ -1,4 +1,5 @@
 // Load required modules
+const mongo = require('mongodb').MongoClient;
 var http    = require("http");              // http server core module
 var https = require('https');
 var fs = require('fs');
@@ -7,46 +8,38 @@ var serveStatic = require('serve-static');  // serve static files
 var socketIo = require("socket.io");        // web socket external module
 var easyrtc = require("easyrtc");               // EasyRTC external module
 var dgram = require('dgram')
-const udpServer = dgram.createSocket('udp4');
 var options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
 
 app.use(serveStatic(__dirname, {'index': ['index.html']}));
+var webServer = http.createServer(app).listen(80,"192.168.0.6");
+//var webServer = https.createServer(options, app).listen(443,"192.168.0.6");
+var socketServer = socketIo.listen(webServer, {"log level":1});
 
-var webServer = http.createServer(app);
-//var webServer = https.createServer(options, app);
-//webServer.listen(443,"192.168.0.6");
-webServer.listen(80,"192.168.0.6", function () {});
-
+const udpServer = dgram.createSocket('udp4');
 udpServer.bind(55404,"192.168.0.6");
 
 var CPosX;
 var CPosY;
+var BCIString = String.fromCharCode.apply(null, new Uint16Array(msg1))
+var CPx = BCIString.indexOf("CursorPosX");
+var CPy = BCIString.indexOf("CursorPosY");
+var TarC = BCIString.indexOf("TargetCode");
+var ResC = BCIString.indexOf("ResultCode");
 
 udpServer.on('message', (msg1, rinfo) => {
-  var BCIString = String.fromCharCode.apply(null, new Uint16Array(msg1))
-  var CPx = BCIString.indexOf("CursorPosX");
-  var CPy = BCIString.indexOf("CursorPosY");
-  var TarC = BCIString.indexOf("TargetCode");
-  var ResC = BCIString.indexOf("ResultCode");
   if(CPx == 0){
-    CPosX=parseFloat(BCIString.substring(11,15));
+    CPosX = parseFloat(BCIString.substring(11,15));
   }
   if(CPy == 0){
-    CPosY=parseFloat(BCIString.substring(11,15));
+    CPosY = parseFloat(BCIString.substring(11,15));
   }
   if(CPy == 0 || CPx == 0){
     socketServer.emit('Pos',{ PosX: (CPosX-1548)/387, PosY: ((CPosY-1548)/290)+6.15});
-//    console.log("Cursor position X: ");
-  //  console.log((CPosX));
-    //console.log("Cursor position Y: ");
-    console.log((CPosY));
   }
 });
-
-var socketServer = socketIo.listen(webServer, {"log level":1});
 
 var myIceServers = [
   {"url":"stun:stun.l.google.com:19302"},
@@ -73,5 +66,3 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
     rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
     });
 });
-
-//listen on port
